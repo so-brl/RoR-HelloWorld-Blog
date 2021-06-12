3f6f6770f05f766bcf61d268dae523fd57233de4
so-brl <Ca167043Soso12@>
2021-06-12 21:00:52 +0200

    INIT first commit

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ApplicationRecord
+  include Visible
+
+  has_many :comments, dependent: :destroy
+  has_one_attached :image
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+  validates :image, presence: true
+
+end
